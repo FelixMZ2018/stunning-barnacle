ddd4b00cbc4db4d5b1a8dc7aa6d35c58f7e15e26
Felix <FelixinLondon@googlemail.com>
2021-02-10 16:26:13 +0100

    Adding Status API Adding rspec framework

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get 'status', to: 'app#status'
       get 'messages/index', to: 'messages#index'
       post 'messages/create', to: 'messages#create'
       get 'messages/show/:id', to: 'messages#show'
